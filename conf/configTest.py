5e8c39b18e98f9826ec4026dffb6af259aabe84c
DESKTOP-C21C1Q8\tangs <tangscna@gmail.com>
2018-12-31 15:03:00 +0800

    Add test case with package config and rename config to conf

--- conf/configTest.py
+++ conf/configTest.py
@@ -1,8 +1,9 @@
+import ast
 import unittest
 
 import os
 
-import config
+from conf import config
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,11 +12,19 @@ class MyTestCase(unittest.TestCase):
         path = os.path.dirname(__file__) + '/test/user.conf'
         conf = config.new_config(path)
         conf.print()
-        self.assertEqual(len(conf.info), 4)
+        self.assertEqual(len(conf.info), 5)
         self.assertEqual(conf.info["key1"], 'key1')
         self.assertEqual(conf.info["key2"], '')
         self.assertEqual(conf.info["key3"], 'key3')
         self.assertEqual(conf.info["key4"], '"http://www.baidu.com"')
+        self.assertEqual(conf.info["key5"], '{"a":"a", "b": "c", "c": 1}')
+
+        key5_string = conf.info["key5"]
+        my_dict = ast.literal_eval(key5_string)
+        self.assertIsInstance(my_dict, dict)
+        self.assertEqual(my_dict["a"], "a")
+        self.assertEqual(my_dict["b"], "c")
+        self.assertEqual(my_dict["c"], 1)
 
         # Configuration file is not exist.
         path = "test.conf"
